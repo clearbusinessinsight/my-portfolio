9767cd0dc1928935fbcee2eb24c7dff65676f194
clearbusinessinsight <choltz@cacbydesign.com>
2022-12-04 21:31:54 -0500

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+const path = window.location.pathname
+    return (
+        <nav className="nav">
+            <Link to="/" className="site-title" >
+                Chris Holtz Portfolio
+                </Link>
+            <ul>
+                <CustomLink to="/projects">Projects</CustomLink>
+                <CustomLink to="/contact">Contact</CustomLink>
+                <CustomLink to="/about">About Me</CustomLink>
+
+
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+   
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+  
+    )
+}
+    
