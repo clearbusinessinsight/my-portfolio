fe4831ffc8fcfcddfe3b26eecf18f89ce5f96186
clearbusinessinsight <choltz@cacbydesign.com>
2022-12-09 14:26:04 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
 import React from 'react'
-import { Container  } from 'react-bootstrap'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
-import Testimonials from './pages/Testimonials'
 import { Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
-
 function App() {
-    
+   
   return (
     <>
+      <Header />
+    
       <div className='bg-gradient-to-b from-black to-gray-800 md:h-screen text-white'>
-
-        <Header />
-          <main>
-        <Routes>
+        <main>
+          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/skills" element={<Skills />} />
-            <Route path="/testimonials" element={<Testimonials />} />
-        </Routes>
+          </Routes>
+            
        
-                  
-      </main>
-      
-      <Footer />
-    </div>
+          <Footer />
+        </main>
+      </div>
     </>
   )
 }
-
+  
+ 
+ 
 export default App
+
